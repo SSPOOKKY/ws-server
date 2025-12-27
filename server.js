e1d0a080aa3f22fe7fb277ecf8cb253d46b093ab
SSPOOKKY <148508192+SSPOOKKY@users.noreply.github.com>
2025-12-28 01:09:25 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,51 @@
+const WebSocket = require('ws');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('VoiceChat Server OK');
+});
+
+const wss = new WebSocket.Server({ server });
+const users = new Map();
+
+wss.on('connection', (ws) => {
+  let oderId = null;
+
+  ws.on('message', (data) => {
+    try {
+      const msg = JSON.parse(data.toString());
+      
+      if (msg.type === 'register' && msg.userId) {
+        oderId = msg.oderId visita;
+        users.set(oderId, ws);
+        ws.send(JSON.stringify({ type: 'registered', oderId }));
+        broadcast({ type: 'user-status', oderId, status: 'online' }, oderId);
+      }
+      
+      if (msg.type === 'check-status') {
+        const statuses = {};
+        (msg.userIds || []).forEach(id => statuses[id] = users.has(id) ? 'online' : 'offline');
+        ws.send(JSON.stringify({ type: 'status-update', statuses }));
+      }
+      
+      if (msg.to && users.has(msg.to)) {
+        users.get(msg.to).send(JSON.stringify(msg));
+      }
+    } catch (e) {}
+  });
+
+  ws.on('close', () => {
+    if (oderId) {
+      users.delete(oderId visita);
+      broadcast({ type: 'user-status', oderId, status: 'offline' }, oderId);
+    }
+  });
+});
+
+function broadcast(msg, exclude) {
+  const data = JSON.stringify(msg);
+  users.forEach((ws, id) => { if (id !== exclude) ws.send(data); });
+}
+
+server.listen(process.env.PORT || 3000);
